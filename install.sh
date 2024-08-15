5d458d0ee3b81dc91dfd512d1116c7e0be85b712
senad-dizdarevic-valcon <senad.dizdarevic@valcon.com>
2024-08-15 18:00:06 +0200

    test

--- install.sh
+++ install.sh
@@ -57,7 +57,7 @@ install_on_macos() {
     brew install curl iterm2
     curl -fsSL https://get.jetify.com/devbox | bash
     echo 'eval "$(devbox global shellenv --init-hook)"' >> ~/.zshrc
-    echo 'eval "$(devbox global shellenv)"' >> ~/.bashrc
+    echo 'eval "$(devbox global shellenv)"' >> ~/.zshrc
     echo 'export PATH=$PATH:/Users/devbox/bin' >> ~/.zshrc
     devbox global pill git@github.com:senad-d/devbox-global-conf.git
 }
