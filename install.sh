aadb7b2af8276ac4f118169e135aabb3ce555ac8
senad-dizdarevic-valcon <senad.dizdarevic@valcon.com>
2024-08-15 17:47:04 +0200

    fix

--- install.sh
+++ install.sh
@@ -54,19 +54,16 @@ install_on_macos() {
     echo "Installing on MacOS..."
     
     brew update
-    echo "Installing Iterm2"
     brew install curl iterm2
-    echo "Installing VSCode"
     brew install --cask visual-studio-code
-    echo "Installing Nix"
     sh <(curl -L https://nixos.org/nix/install)
-    echo "Installing DevBox"
     curl -fsSL https://get.jetify.com/devbox | bash
-    sudo echo 'eval "$(devbox global shellenv --init-hook)"' >> ~/.zshrc
-    sudo echo 'export PATH=$PATH:/Users/devbox/bin' >> ~/.zshrc
-    echo "Installing ohmyzsh"
+    echo 'eval "$(devbox global shellenv --init-hook)"' >> ~/.zshrc
+    echo 'eval "$(devbox global shellenv)"' >> ~/.bashrc
+    echo 'export PATH=$PATH:/Users/devbox/bin' >> ~/.zshrc
+    echo "=====> Installing ohmyzsh"
     sudo sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" --unattended --keep-zshrc
-    echo "Installing powerlevel10k"
+    echo "=====> Installing powerlevel10k"
     sudo git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
     sudo sed -i 's/^ZSH_THEME=.*/ZSH_THEME="powerlevel10k\/powerlevel10k"/' ~/.zshrc
     p10k configure
